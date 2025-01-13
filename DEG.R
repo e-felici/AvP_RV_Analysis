52e896a638936ac5bb99fcbf81dbcda70f5b681b
María Esperanza Felici <163751874+e-felici@users.noreply.github.com>
2025-01-13 18:06:14 -0300

    Add files via upload

---
+++ DEG.R
@@ -0,0 +1,66 @@
+#!/usr/bin/Rscript
+
+#load library
+library(tidyverse)
+
+# Get the command line arguments
+args <- commandArgs(TRUE)
+
+# Get the folder argument
+subdir <- args[1]
+
+#Here do not forget to define the home directory
+home <- "/path/to/directory/"
+
+cat("Filtering, cleaning, and sorting DEG results\n")
+
+tryCatch({
+  #Read all necessary files:
+      #Raw results
+  raw <- read_tsv(paste0(home, "/WorkDir/", subdir, "/DEG_results/", subdir, "-vs_DEG.out"), col_names = 1:12)
+      #Protein IDs
+  ids <- read_tsv(paste0(home, "/WorkDir/", subdir, "/AllProteinIds-",subdir,".txt"), col_names = "ID")
+
+  # Define the criteria for filtering the results
+  index = ifelse(((raw$X11 < 1e-5) + (raw$X12>50) + (raw$X3 > 25)) == 3, TRUE, FALSE)
+  
+  # Filter the raw data based on the defined criteria
+  out = raw[index, c(1, 2, 3, 11, 12)]
+  out = out %>% as_tibble()
+  
+  #Polish data
+  colnames(out)[1:5] = c("ID","Essential.Homologue","Essential.IdentityPercent",
+                         "Essential.Evalue","Essential.Bitscore")
+                        
+  # Create data frame with proteins that were not classified as essential
+  MissingIDs <- ids[!(ids$ID %in% out$ID), ]
+  df <- data.frame(matrix("-", nrow = nrow(MissingIDs), ncol = 4))
+  colnames(df) <- c("Essential.Homologue","Essential.IdentityPercent",
+                    "Essential.Evalue","Essential.Bitscore")
+  MissingIDs <- cbind(MissingIDs, df)
+  
+  # Combine the raw results with the missing proteins
+  AllResults <- rbind(out, MissingIDs)
+  
+  # Arrange the combined data frame. Keep only distinct proteins
+  AllResults <- arrange(AllResults, ID, Essential.Bitscore)%>%
+    distinct(ID, .keep_all = TRUE)
+  
+  #Polish final results
+  AllResults$ID <- AllResults$ID %>% str_replace("\\.1", "")
+  
+  # Create essential protein column
+  AllResults$EssentialProtein = ifelse(AllResults$Essential.Homologue == "-",
+                                       "Non Essential",
+                                       "Essential")
+  
+  
+  # Write the final results to a TSV file
+  write_tsv(AllResults, paste0(home, "/WorkDir/", subdir,"/Final_results/DEG-", subdir, "-final.tsv"))
+  rm(list = ls())
+}, 
+# Catch any errors that may occur during execution
+error = function(e) {
+  # Print a warning message if an error occurs
+  warning(paste0("Error arranging DEG results: ", conditionMessage(e)))
+})
