74c54e901840d043226b1f520cac9a2230eef8b5
María Esperanza Felici <163751874+e-felici@users.noreply.github.com>
2025-01-15 09:19:21 -0300

    Update 0.DB_creation.sh
    Add description

--- 0.DB_creation.sh
+++ 0.DB_creation.sh
@@ -1,9 +1,32 @@
 #!/bin/bash
 
+# =====================================================================
+# Script Name:    0.DB_creation.sh
+# Description:    Before performing blastp, We must create the databases for all the blastp comparisons.
+#                 So, this is what this script does.
+#                 For the AvP antigen search, we created the databases for the fasta files stored in all
+#                 these folders:
+#                    -Bos_taurus
+#                    -Felis_silvestris_catus
+#                    -Canis_familiaris
+#                    -Homo_sapiens
+#                    -Ovis_orientalis_aries
+#                    -Capra_aegagrus_hircus
+#                    -Mus_musculus
+#                    -Sus_scrofa
+#                    -Equus_ferus_caballus
+#                    -Oncorhynchus_mykiss
+#                    -DEG_bacteria
+#                    -VFDB_full
+#                 For more details, see: https://www.ncbi.nlm.nih.gov/books/NBK279690/ 
+# Author:         M. Esperanza Felici
+# Usage:          ./0.DB_creation.sh
+# =====================================================================
+
+
 
 echo "************* BLASTp Databases Creation  *************"
 
-#Before performing blastp, We must create the databases. See: https://www.ncbi.nlm.nih.gov/books/NBK279690/ 
 #Define the directory for the databases. This is the directory where each folder has a fasta file for the database creation
 DBDIR="/path/to/your/directory"
 
