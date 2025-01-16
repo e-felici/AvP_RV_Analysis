5745ff5aa6f653ad0b5f54ea24fa0aad11e39cb6
María Esperanza Felici <163751874+e-felici@users.noreply.github.com>
2025-01-16 10:33:54 -0300

    MAJOR Update SPAAN.R

--- RScripts/SPAAN.R
+++ RScripts/SPAAN.R
@@ -1,32 +1,4 @@
 #!/usr/bin/Rscript
-
-if (!requireNamespace("dplyr", quietly = TRUE)) {
-  install.packages("dplyr")
-}
-if (!requireNamespace("tidyr", quietly = TRUE)) {
-  install.packages("tidyr")
-}
-if (!requireNamespace("purrr", quietly = TRUE)) {
-  install.packages("purrr")
-}
-if (!requireNamespace("readr", quietly = TRUE)) {
-  install.packages("readr")
-}
-
-if (!requireNamespace("stringr", quietly = TRUE)) {
-  install.packages("stringr")
-}
-if (!requireNamespace("tibble", quietly = TRUE)) {
-  install.packages("tibble")
-}
-if (!requireNamespace("magrittr", quietly = TRUE)) {
-  install.packages("magrittr")
-}
-if (!requireNamespace("forcats", quietly = TRUE)) {
-  install.packages("forcats")
-}
-
-
 #load library
 library("dplyr")
 library("tidyr")
@@ -37,25 +9,23 @@ library("stringr")
 library("forcats")
 library("magrittr")
 
-
-
 # Get the command line arguments
-args <- commandArgs(TRUE)
+args <- commandArgs(trailingOnly = TRUE)  # Capture arguments
+if (length(args) != 2) {
+  stop("Two arguments are required!")
+}
 
-# Get the folder argument
-subdir <- args[1]
-
-#Here do not forget to define the home directory
-home <- "/path/to/directory"
+MAIN <- args[1]
+subdir <- args[2]
 
 cat("Filtering, cleaning, and sorting the results of SPAAN\n")
 
 tryCatch({
   #Read all necessary files:
       #Raw results
-  raw = read_tsv(paste0(home, "/WorkDir/", subdir, "/SPAAN_results/SPAAN-unpolished.txt"))
+  raw = read_tsv(paste0(MAIN, "/", subdir, "/SPAAN_results/SPAAN-unpolished.txt"))
       #Protein IDs
-  ids <- read_tsv(paste0(home, "/WorkDir/", subdir, "/AllProteinIds-",subdir,".txt"), col_names = "ID")
+  ids <- read_tsv(paste0(MAIN, "/", subdir, "/AllProteinIds-",subdir,".txt"), col_names = "ID")
   colnames(ids)[1] <- "ID"
   
   #Polish IDs
@@ -82,7 +52,7 @@ tryCatch({
   
   
   # Write the final results to a TSV file
-  write_tsv(raw, paste0(home, "/WorkDir/", subdir, "/Final_results/SPAAN-", subdir, "-final.tsv"))
+  write_tsv(raw, paste0(MAIN, "/", subdir, "/Final_results/SPAAN-", subdir, "-final.tsv"))
   rm(list = ls())
 }, 
 # Catch any errors that may occur during execution
