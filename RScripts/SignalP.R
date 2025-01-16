3f89e23302c76e5b147f0db4e2380027811d0196
María Esperanza Felici <163751874+e-felici@users.noreply.github.com>
2025-01-16 10:36:11 -0300

    MAJOR Update SignalP.R

--- RScripts/SignalP.R
+++ RScripts/SignalP.R
@@ -1,33 +1,5 @@
 #!/usr/bin/Rscript
 
-
-if (!requireNamespace("dplyr", quietly = TRUE)) {
-  install.packages("dplyr")
-}
-if (!requireNamespace("tidyr", quietly = TRUE)) {
-  install.packages("tidyr")
-}
-if (!requireNamespace("purrr", quietly = TRUE)) {
-  install.packages("purrr")
-}
-if (!requireNamespace("readr", quietly = TRUE)) {
-  install.packages("readr")
-}
-
-if (!requireNamespace("stringr", quietly = TRUE)) {
-  install.packages("stringr")
-}
-if (!requireNamespace("tibble", quietly = TRUE)) {
-  install.packages("tibble")
-}
-if (!requireNamespace("magrittr", quietly = TRUE)) {
-  install.packages("magrittr")
-}
-if (!requireNamespace("forcats", quietly = TRUE)) {
-  install.packages("forcats")
-}
-
-
 #load library
 library("dplyr")
 library("tidyr")
@@ -38,22 +10,20 @@ library("stringr")
 library("forcats")
 library("magrittr")
 
-
-
 # Get the command line arguments
-args <- commandArgs(TRUE)
+args <- commandArgs(trailingOnly = TRUE)  # Capture arguments
+if (length(args) != 2) {
+  stop("Two arguments are required!")
+}
 
-# Get the folder argument
-subdir <- args[1]
-
-#Here do not forget to define the home directory
-home <- "/path/to/directory/"
+MAIN <- args[1]
+subdir <- args[2]
 
 cat("Filtering, cleaning, and sorting the results of the homology analysis\n")
 
 tryCatch({
   #Read raw results
-  lines <- as_tibble(read_tsv(paste0(home, "/WorkDir/", subdir, "/SignalP_results/prediction_results.txt"), skip = 1))
+  lines <- as_tibble(read_tsv(paste0(MAIN, "/", subdir, "/SignalP_results/prediction_results.txt"), skip = 1))
   
   #Polish results
   colnames(lines)[1:2] = c("ID", "SignalPeptide")
@@ -62,7 +32,7 @@ tryCatch({
   lines <- lines %>% mutate(ID = sub(" .*", "", ID))
   
   # Write the final results to a TSV file
-  write_tsv(lines, paste0(home, "/WorkDir/", subdir, "/Final_results/SignalP-", subdir, "-final.tsv"))
+  write_tsv(lines, paste0(MAIN, "/", subdir, "/Final_results/SignalP-", subdir, "-final.tsv"))
   rm(list = ls())
   
   }, error = function(e) {
