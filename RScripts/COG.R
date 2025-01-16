a38986a3c4c099b79692efb83c8ec383ab836648
María Esperanza Felici <163751874+e-felici@users.noreply.github.com>
2025-01-16 10:11:33 -0300

    MAJOR Update COG.R

--- RScripts/COG.R
+++ RScripts/COG.R
@@ -1,32 +1,5 @@
 #!/usr/bin/Rscript
 
-
-if (!requireNamespace("dplyr", quietly = TRUE)) {
-  install.packages("dplyr")
-}
-if (!requireNamespace("tidyr", quietly = TRUE)) {
-  install.packages("tidyr")
-}
-if (!requireNamespace("purrr", quietly = TRUE)) {
-  install.packages("purrr")
-}
-if (!requireNamespace("readr", quietly = TRUE)) {
-  install.packages("readr")
-}
-if (!requireNamespace("stringr", quietly = TRUE)) {
-  install.packages("stringr")
-}
-if (!requireNamespace("tibble", quietly = TRUE)) {
-  install.packages("tibble")
-}
-if (!requireNamespace("magrittr", quietly = TRUE)) {
-  install.packages("magrittr")
-}
-if (!requireNamespace("forcats", quietly = TRUE)) {
-  install.packages("forcats")
-}
-
-
 #load library
 library("dplyr")
 library("tidyr")
@@ -38,45 +11,46 @@ library("forcats")
 library("magrittr")
 
 # Get the command line arguments
-args <- commandArgs(TRUE)
+args <- commandArgs(trailingOnly = TRUE)  # Capture arguments
+if (length(args) != 3) {
+  stop("Three arguments are required!")
+}
 
-# Get the folder argument
-subdir <- args[1]
-
-#Here do not forget to define the home directory
-home <- "/path/to/directory/"
+MAIN <- args[1]
+subdir <- args[2]
+COG <- args[3]
 
 cat("Filtering, cleaning, and sorting COG results\n")
 
 tryCatch({
   #Read all necessary files
   ##Raw rps-blast results
-  AllResults = read_tsv(paste0(home, "/WorkDir/", subdir, "/COG_results/", subdir, "-vs_COG.out"), 
+  AllResults = read_tsv(paste0(MAIN, "/", subdir, "/COG_results/", subdir, "-vs_COG.out"), 
                         col_names = 1:12)
   AllResults$X1 <- AllResults$X1 %>% str_replace("\\.1", "")
   
   ##Protein Ids
-  AllProteinIds = read_tsv(paste0(home, "/WorkDir/", subdir, "/AllProteinIds-", subdir, ".txt"),
+  AllProteinIds = read_tsv(paste0(MAIN, "/", subdir, "/AllProteinIds-", subdir, ".txt"),
                            col_names = "ID")
   AllProteinIds$ID <- AllProteinIds$ID %>% str_replace("\\.1", "")
   
   ##COG and CDD resources:
   #Summary info about the CD models
-  cddid <- as.tibble(read_tsv(paste0(home, "/COG/cddid.tbl"), 
+  cddid <- as.tibble(read_tsv(paste0(COG, "/cddid.tbl"), 
                               col_names = F))
   #Polish tibble
   colnames(cddid)[1] = "CDDID"
   cddid$CDDID <- as.character(cddid$CDDID)
   
   #Info about COG descriptions
-  cogdef <- as.tibble(read_tsv(paste0(home, "/COG/cog-20.def.tab"), 
+  cogdef <- as.tibble(read_tsv(paste0(COG, "/cog-20.def.tab"), 
                                col_names = F))
   #Polish tibble
   cogdef <- select(cogdef, X1, X2)
   colnames(cogdef)[1] = "COGID"
   
   #Descriptions of COG functional categories
-  cogdet <- as.tibble(read_tsv(paste0(home, "/COG/fun-20.tab"), 
+  cogdet <- as.tibble(read_tsv(paste0(COG, "/fun-20.tab"), 
                                col_names = F))
   #Polish tibble
   colnames(cogdet)[1] = "COG"
@@ -142,7 +116,7 @@ tryCatch({
     distinct(ID, .keep_all = TRUE)
   
   # Write the final results to a TSV file
-  write_tsv(AllResults, paste0(home, "/WorkDir/", subdir, "/Final_results/COG-", subdir, "-final.tsv"),
+  write_tsv(AllResults, paste0(MAIN, "/", subdir, "/Final_results/COG-", subdir, "-final.tsv"),
             na = "-")
   
   rm(list = ls())
